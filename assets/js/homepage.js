c2d53be7d7bcb518625459767e8fea75bbe1af42
Jonathan Cano <jcano528@gmail.com>
2021-08-12 00:22:33 -0500

    learning some shhh

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,33 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-        console.log("inside", response);
+var getUserRepos = function(user) {
+    fetch("https://api.github.com/users/" + user + "/repos").then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
     });
 
     console.log("outside");
 };
 
-getUserRepos();
+// getUserRepos();
 
-// asynchronous behavior
+document.querySelector("#submit-btn").addEventListener("click", function(event) {
+    // only called when user clicks the button
+    console.log("hello 1");
+});
+   
+setTimeout(function() {
+// called after 5 seconds, regardless if button was clicked
+console.log("hello 2");
+}, 5000);
+
+// called immediately on page load
+console.log("hello 3");
+
+// You can liken an API endpoint to a function in the sense that
+// the same base URL can return different data depending on the argument(s) given.
+// Consider the following examples:
+getUserRepos("octocat"); // https://api.github.com/users/octocat/repos
+getUserRepos("microsoft"); // https://api.github.com/users/microsoft/repos
+
+// Or more generically:
+// getUserRepos(user); // https://api.github.com/users/<user>/repos
